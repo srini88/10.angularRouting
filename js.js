5f6e89f0dcbe1caff15b1ed4515d6bcca8c107ff
srini88 <pvsrinivas.ece@gmail.com>
2016-06-07 12:57:28 -0500

    all working someerrors in console..redirectTo

--- js.js
+++ js.js
@@ -7,8 +7,20 @@
 			.when('/',{
 				controller:'HomeController',
 				controllerAs:'home',
-				template:'<h1>Hello routing</h1>'
-				//templateUrl:'templates/home.html'
+				templateUrl:'templates/home.html'
+			}).when('/schools',{
+				controller:'AllSchoolsController',
+				controllerAs:'schools',
+				templateUrl:'templates/allSchools.html'
+			}).when('/classrooms',{
+				controller:'AllClassroomsController',
+				controllerAs:'classrooms',
+				templateUrl:'templates/allClassrooms.html',
+				redirectTo:'/'
+			}).when('/activities',{
+				controller:'AllActivitiesController',
+				controllerAs:'activities',
+				templateUrl:'templates/allActivities.html'
 			});
 	}])
 	app.controller('HomeController', ['$scope', function($scope){
